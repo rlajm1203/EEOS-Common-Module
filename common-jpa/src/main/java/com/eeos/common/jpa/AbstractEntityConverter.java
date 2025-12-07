28ccfe630882b48cdc863d92517e832ad9e8defa
Bellmin <whdals011203@naver.com>
2025-12-07 23:22:15 +0900

    comment: 주석 추가

--- common-jpa/src/main/java/com/eeos/common/jpa/AbstractEntityConverter.java
+++ common-jpa/src/main/java/com/eeos/common/jpa/AbstractEntityConverter.java
@@ -3,6 +3,7 @@ package com.eeos.common.jpa;
 
 import com.eeos.common.domain.AbstractModel;
 
+// JpaEntity <-> DomainModel 간 변환을 위한 Converter
 public interface AbstractEntityConverter<T extends BaseEntity, R extends AbstractModel> {
     R from(T t);
 
