e03c9643bf4b82e8d8bde487378139bd159ab790
Bellmin <whdals011203@naver.com>
2025-12-07 23:27:23 +0900

    comment: 주석 추가

--- common-web/src/main/java/com/eeos/common/web/dto/AbstractWebDto.java
+++ common-web/src/main/java/com/eeos/common/web/dto/AbstractWebDto.java
@@ -2,6 +2,7 @@ package com.eeos.common.web.dto;
 
 import com.eeos.common.application.AbstractApplicationDto;
 
+// T 타입으로 변환할 수 있는 Web 계층의 DTO
 public interface AbstractWebDto<T extends AbstractApplicationDto> {
     T toApplicationRequest();
 }
